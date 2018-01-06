b3e8fd3d590ad066cdc3c5ad95d96c2b2cd439e1
ericshiu <as1102511@gmail.com>
2018-01-06 20:52:03 +0800

    修正form表單name

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -27,8 +27,8 @@ public class UserController {
 	 */
 	@RequestMapping(value = "login.do", method = RequestMethod.POST)
 	@ResponseBody
-	public ServerResponse<User> login(String userName, String passWord, HttpSession session) {
-		ServerResponse<User> response = iUserService.login(userName, passWord);
+	public ServerResponse<User> login(String username, String password, HttpSession session) {
+		ServerResponse<User> response = iUserService.login(username, password);
 		if (response.isSuccess()) {
 			session.setAttribute(Const.CURRENT_USER, response.getData());
 		}
